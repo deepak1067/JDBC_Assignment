91b24643c49da74348c50493c0ebbe350cb1ab23
deepak1067 <88529321+deepak1067@users.noreply.github.com>
2022-01-05 12:09:24 +0530

    JDBC_Assignment

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,4 +1,7 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class JDBC {
     public static void main(String[] args) {
         try
